7f813cd469e2b09776e4b381647b821ee1d8212a
DAVID AMAYUELAS <amayudance@gmail.com>
2017-03-31 17:17:54 +0200

    Prueba lectura

--- messages/index.js
+++ messages/index.js
@@ -1,7 +1,7 @@
 "use strict";
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
-
+const fs=require('fs');
 var useEmulator = (process.env.NODE_ENV == 'development');
 
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
@@ -15,7 +15,17 @@ var bot = new builder.UniversalBot(connector);
 
 bot.dialog('/', function (session) {
     session.send('You said ' + session.message.text);
-});
+},
+function(session){
+    var pregunta=fs.createReadStream('q8.txt', {start: 0, end: 64});
+    session.send(pregunta);
+    builder.Prompts.choice(session,'Select an option: ',[a,b,c,d]);
+},
+function(session,results){
+    session.user.data=results.response;
+    session.send('Has seleccionado la opcion '+session.user.data);
+}
+);
 
 if (useEmulator) {
     var restify = require('restify');
