594ccab8971448ae8a946542cb1661e32bdff9c5
DAVID AMAYUELAS <amayudance@gmail.com>
2017-04-04 17:18:49 +0200

    a ver si va

--- messages/index.js
+++ messages/index.js
@@ -10,34 +10,46 @@ var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure
     stateEndpoint: process.env['BotStateEndpoint'],
     openIdMetadata: process.env['BotOpenIdMetadata']
 });
-var bot = new builder.UniversalBot(connector);
-
-bot.dialog('/', [function (session) {
-    builder.Prompts.text(session,'Bienvenido al bot GetTalent,¿Como te llamas?.');
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.beginDialog('q&aDialog');
     },
     function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.text(session, 'Hola ' +session.userData.name+ ', Indica cuantas preguntas puedo hacerte:');
-    },
-    function (session, results) {
-        var cant=results.response;
-        for(var i=1;i<=cant;i++){
-            pregunta(i);
-            var respuesta=results.response;
-            session.endDialog();
-            //Aquí pondríamos el archivo de texto de destino
-        };
+        session.send("Thanks %(name)s... You're %(age)s and located in %(state)s.", results.response);
     }
-    //Los datos los tengo guardados para usos posteriores
 ]);
-function pregunta(i){
-    session.send('Pregunta '+i+' :');
-    session.beginDialog('/pregunta');
-}
-bot.dialog('/pregunta',function (session) {
-        builder.Prompts.choice(session,"Si tuvieras que elegir entre estos colores cual elegírias:",['Rojo','Amarrillo','Verde','Azul']);
-});
 
+// Add Q&A dialog
+bot.dialog('q&aDialog', [
+    function (session, args) {
+        // Save previous state (create on first call)
+        session.dialogData.index = args ? args.index : 0;
+        session.dialogData.form = args ? args.form : {};
+
+        // Prompt user for next field
+        builder.Prompts.text(session, questions[session.dialogData.index].prompt);
+    },
+    function (session, results) {
+        // Save users reply
+        var field = questions[session.dialogData.index++].field;
+        session.dialogData.form[field] = results.response;
+
+        // Check for end of form
+        if (session.dialogData.index >= questions.length) {
+            // Return completed form
+            session.endDialogWithResult({ response: session.dialogData.form });
+        } else {
+            // Next field
+            session.replaceDialog('q&aDialog', session.dialogData);
+        }
+    }
+]);
+
+var questions = [
+    { field: 'name', prompt: "What's your name?" },
+    { field: 'age', prompt: "How old are you?" },
+    { field: 'state', prompt: "What state are you in?" }
+];
 if (useEmulator) {
     var restify = require('restify');
     var server = restify.createServer();
