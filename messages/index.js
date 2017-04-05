83b9fb7a9aedd46eb55e2ce6ba0fb15181cdaf05
DAVID AMAYUELAS <amayudance@gmail.com>
2017-04-05 18:10:15 +0200

    pruebas

--- messages/index.js
+++ messages/index.js
@@ -59,7 +59,7 @@ bot.dialog('/preguntas', [
 var questions = [
     { field: 'num', prompt: "Por favor, indica cuantas preguntas quieres que te haga:" },
     { field: 'question', prompt: "¿Cuanto es 1+1?",Prompts:"Pizza|Drinks|Extras"},
-    { field: 'question2',builder.Prompts.choice() },
+    { field: 'question2', prompt:"¿Cuanto es 1+1?"},
     { field: 'question3', prompt:(builder.Prompts.choice(session,"¿Cuanto es 1+1?",['1','2','3'])) },
     { field: 'question4', prompt: "¿Cuanto es 1+1?" },
     { field: 'question5', prompt: "¿Cuanto es 1+1?" },
