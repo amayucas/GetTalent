1c141586e3162f8acd300722d9293b343569c7b8
DAVID AMAYUELAS <amayudance@gmail.com>
2017-04-07 18:20:32 +0200

    madre

--- messages/index.js
+++ messages/index.js
@@ -96,7 +96,7 @@ var questions = [
     { field: 'question1', prompt: "Which of these is used to access member of class before object of that class is created?",choices: "public|private|static|protected"},
     { field: 'question2', prompt:"Which of these is used as default for a member of a class if no access specifier is used for it?",choices: "private|public|public, within its own package|protected"},
     { field: 'question3', prompt:"What is the process by which we can control what parts of a program can access the members of a class?" ,choices:"Polymorphism|Abstraction|Encapsulation|Recursion"},
-    { field: 'question4', prompt: "Ya llevamos "+session.dialogData.index+" preguntas.",choices:"Continuar|Salir"},
+    { field: 'question4', prompt: "¿Continuamos con el test?.",choices:"Continuar|Salir"},
     { field: 'question5', prompt: "¿Cuanto es 1+1?",choices:"1|2|3|4"},
     { field: 'question6', prompt: "¿Cuanto es 1+1?",choices:"1|2|3|4"},
     { field: 'question7', prompt: "¿Cuanto es 1+1?",choices:"1|2|3|4"},
