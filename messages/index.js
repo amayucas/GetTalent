36d1edefbd9c6ea986cecd479f4d0cd11d59092e
DAVID AMAYUELAS <amayudance@gmail.com>
2017-04-07 10:53:31 +0200

    joder

--- messages/index.js
+++ messages/index.js
@@ -44,7 +44,7 @@ bot.dialog('/inicio', [
                 callback(null, 1.0, { topic: 'general' });
                 break;
             case 'salir':
-                callback();
+                callback(null,2.0);
                 break;
             default:
                 callback(null, 0.0);
@@ -74,70 +74,61 @@ bot.dialog('/preguntas', [
         }
     }
 ]);
+
 var questions = [
-    { field: 'num', prompt: "Por favor, indica cuantas preguntas quieres que te haga:",choices:"1|2|3" },
-    { field: 'question', prompt: "¿Cuanto es 1+1?",choices: "1|2|3|4"},
-    { field: 'question2', prompt:"¿Cuanto es 1+1?",choices: "1|2|3|4"},
-    { field: 'question3', prompt:"¿Cuanto es 1+1?" ,choices:""},
-    { field: 'question4', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question5', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question6', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question7', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question8', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question9', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question10', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question11', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question12', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question13', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question14', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question15', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question16', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question17', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question18', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question19', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question20', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question21', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question22', prompt: "¿Cuanto es 1+1?"},
-    { field: 'question23', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question24', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question25', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question26', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question27', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question28', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question29', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question30', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question31', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question32', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question33', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question34', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question35', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question36', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question37', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question38', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question39', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question40', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question41', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question42', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question43', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question44', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question45', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question46', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question47', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question48', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question49', prompt: "¿Cuanto es 1+1?" },
-    { field: 'question50', prompt: "¿Cuanto es 1+1?" }
+    { field: 'question', prompt: "Which of these access specifiers must be used for main() method? ",choices:"private|public|protected|None of the mentioned"},
+    { field: 'question1', prompt: "Which of these is used to access member of class before object of that class is created?",choices: "public|private|static|protected"},
+    { field: 'question2', prompt:"Which of these is used as default for a member of a class if no access specifier is used for it?",choices: "private|public|public, within its own package|protected"},
+    { field: 'question3', prompt:"What is the process by which we can control what parts of a program can access the members of a class?" ,choices:"Polymorphism|Abstraction|Encapsulation|Recursion"},
+    { field: 'question4', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question5', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question6', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question7', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question8', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question9', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question10', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question11', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question12', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question13', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question14', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question15', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question16', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question17', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question18', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question19', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question20', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question21', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question22', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question23', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question24', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question25', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question26', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question27', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question28', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question29', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question30', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question31', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question32', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question33', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question34', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question35', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question36', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question37', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question38', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question39', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question40', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question41', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question42', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question43', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question44', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question45', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question46', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question47', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question48', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question49', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"},
+    { field: 'question50', prompt: "¿Cuanto es 1+1?",choices:"1|2|2|4"}
 ];
-bot.on('addbot', function (message) {
-    if (message.action === 'add') {
-        var name = message.user ? message.user.name : null;
-        var reply = new builder.Message()
-                .address(message.address)
-                .text("Hola %s... Gracias por añadirme. Di 'Hola' para ver una demo", name || 'there');
-        bot.send(reply);
-    } else {
-        // delete their data
-    }
-});
+
 bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
 if (useEmulator) {
     var restify = require('restify');
