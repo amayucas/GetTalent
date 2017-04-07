ec6ba633244f84518b48f459f83a101516957d03
DAVID AMAYUELAS <amayudance@gmail.com>
2017-04-07 10:56:28 +0200

    callback

--- messages/index.js
+++ messages/index.js
@@ -44,7 +44,7 @@ bot.dialog('/inicio', [
                 callback(null, 1.0, { topic: 'general' });
                 break;
             case 'salir':
-                callback(null,2.0);
+                callback(null,-1.0);
                 break;
             default:
                 callback(null, 0.0);
